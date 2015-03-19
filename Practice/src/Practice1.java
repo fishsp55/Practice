7b950e7daa923d57325840ec29dcd7e39443b599
Faith <fcollins@Faith-Collinss-MacBook-Pro.local>
2015-03-19 09:04:21 -0400

    Initial commit

---
+++ Practice/src/Practice1.java
@@ -0,0 +1,13 @@
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		for (int i = 0; i < num; i++){
+			System.out.println("The current number is " + num);
+		}
+
+	}
+
+}
